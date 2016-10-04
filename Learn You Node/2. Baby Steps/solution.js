b85705e8b2f195c7abc229277978151bd2418f1f
“John <johnjamesmartin@live.com>
2016-10-04 03:45:01 +0100

    First two exercises

---
+++ Learn You Node/2. Baby Steps/solution.js
@@ -0,0 +1,12 @@
+/*
+    "Write a program that accepts one or more numbers as command-line arguments  
+    and prints the sum of those numbers to the console (stdout)."
+*/
+
+var total = 0;
+
+for (var i = 2; i < process.argv[2].length; i++) {
+    total += Number(process.argv[i]);
+}
+
+console.log(total);
